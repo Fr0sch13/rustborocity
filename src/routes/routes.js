1b2f23304e21f1ed35997e98652263f3d8dedd57
Fr0sch13 <60191745+Fr0sch13@users.noreply.github.com>
2020-02-09 13:39:52 -0700

    Setup nodemon and added a test route for Neo4j
    Nodemon is set up, app should be restartable
    Test route set up for a trial run at the DB

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,6 +13,29 @@ var driver = neo4j.driver(
     neo4j.auth.basic('neo4j', 'neo4j')
 )
 
+router.route("/Test").get(
+    function(req,res){
+        const session = driver.session()
+
+        const resultPromise = session.writeTransaction(tx =>
+            tx.run(
+                'CREATE (a:Greeting) SET a.message = $message RETURN a.message + ", from node " + id(a)',
+                { message: 'Hello, world'}
+            )
+        )
+
+        resultPromise.then(result => {
+            session.close()
+
+            const singleRecord = result.records[0]
+            const greeting = singleRecord.get(0)
+
+            console.log(greeting)
+            driver.close()
+        })
+    }
+)
+
 /*
 Making Routes to load my data and get it ready to go
 router.route("/loadData").get(
