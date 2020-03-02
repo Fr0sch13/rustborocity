043fa5ff390dd89384f179b5e2675aecdad92c40
Fr0sch13 <60191745+Fr0sch13@users.noreply.github.com>
2020-03-01 22:19:07 -0700

    Single pokemon backend stuff
    Single pokemon done, query helper function done.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-var databaseName = "pokemon"
+var databaseName = "Pokemon"
 
 /*
 This builds the Neo4j Requirements to talk to the Database. 
@@ -11,7 +11,7 @@ var neo4j = require('neo4j-driver');
 var driver = neo4j.driver(
     'bolt://localhost:7687',
     neo4j.auth.basic('webapp', 'qwer1234')
-)
+);
 
 /*
 / route is landing page
@@ -38,22 +38,78 @@ var menuOptions = [
 
 
 
+// router.route("/").get(
+//     function(req,res){
+//        var bannerIMG = 'public\pictures\testBanner.jpeg'
+//         // var bannerIMG = 'https://fiverr-res.cloudinary.com/image/upload/t_message_attachment_large_thumb,q_auto,f_auto/v1/secured-attachments/message/attachments/03a078fd0b75d3d0216b78140bef690c-1582513677420/rustboroCity_logo_idea.png?__cld_token__=exp=1582853171~hmac=801cb1892d5cbdcfbb4f13e1fc61df373ca0e71408cc703354373220cf938dd9'
+//         var bodyData = {
+//             "img" : bannerIMG,
+//             "paragraph" : "Sample Paragraph"
+//         };
+//         var getNav = req.app.get("getNav");
+//         var model = {
+//             menuOptions: getNav(),
+//             bodyData: bodyData
+//         }
+//         res.render("landing", model);
+//     }
+// )
+
+// router.route("/").get(
+//     function(req,res){
+
+//         var bannerIMG = "";
+        
+//         var bodyData = {
+//             "img" : bannerIMG,
+//             "paragraph" : "Sample Paragraph"
+//         };
+//         var getNav = req.app.get("getNav");
+//         var model = {
+//             menuOptions: getNav(),
+//             bodyData: bodyData
+//         }
+//         res.render("landing", model);
+//     }
+// )
+
 router.route("/").get(
     function(req,res){
-       //var bannerIMG = 'C:/Users/Dalton/Documents/GitHub/rustborocity/public/pictures/testBanner.jpeg'
-        var bannerIMG = 'https://fiverr-res.cloudinary.com/image/upload/t_message_attachment_large_thumb,q_auto,f_auto/v1/secured-attachments/message/attachments/03a078fd0b75d3d0216b78140bef690c-1582513677420/rustboroCity_logo_idea.png?__cld_token__=exp=1582853171~hmac=801cb1892d5cbdcfbb4f13e1fc61df373ca0e71408cc703354373220cf938dd9'
-        var bodyData = {
-            "img" : bannerIMG,
-            "paragraph" : "Sample Paragraph"
-        };
-        var getNav = req.app.get("getNav");
-        var model = {
-            menuOptions: getNav(),
-            bodyData: bodyData
-        }
-        res.render("landing", model);
+        (async function getData(){
+            try{
+                var name = "Bulbasaur"
+                var query = "Match (n:Pokemon {name:\"" + name + "\"}) Return (n) LIMIT 1"
+                var testCount = await hitThatDB(query);
+                console.log(testCount.name + " in slash");
+            }
+            catch(err){
+                console.log(err);
+            }
+        }());
     }
-)
+);
+
+
+async function hitThatDB(query){
+    const session = driver.session();
+    var returnObject = "";
+    return new Promise(resolve => {
+        session.run(query)
+        .then(result => {
+            returnObject = result.records[0]._fields[0].properties;
+            console.log(returnObject);
+        })
+        .catch(e => {
+            console.log(e + " WE MESSED UP!!!!")
+        })
+        .then(() => {
+            session.close();
+            resolve(returnObject);
+        }) 
+    })
+    
+    // return returnObject
+};
 
 /*
 router.route("/Test").get(
