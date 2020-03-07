ea92e7d0a8a80a1489d3c7d93845ce499073fa53
Fr0sch13 <60191745+Fr0sch13@users.noreply.github.com>
2020-03-07 00:03:50 -0700

    working on relations to pokemon

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -70,31 +70,40 @@ router.route("/pokemon").get(
     function(req,res){
         (async function getData(){
             try{
-                var name = "Squirtle";
-                var query = "Match (n:Pokemon {name:\"" + name + "\"}) Return (n)";
+                var name = "Basculin";
+                var query = "Match (p:Pokemon {name:\"" + name + "\"})Return p";
                 var getMon = await hitThatDB(query);
+                
+                var query2 = "Match (p:Pokemon {name:\"" + name + "\"}) Match (p)-[r]-(t) Return p, r, t";
+                var getRelations = await hitThatDbForRelations(query2);
+         
                 var dexNum = getMon.pokemon_id;
                 var pokemonSpriteURL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + dexNum + ".png";
 
                 console.log(pokemonSpriteURL + "\n");
                 console.log(getMon.name + " in slash");
+
+                var sprite = pokemonSpriteURL;
+                var abilities = getMon.abilities.split("', ");
+                var abilityString = await abilityStringMaker(abilities);
             }
             catch(err){
                 console.log(err);
             }
             finally{
-                var bannerIMG = pokemonSpriteURL;
-        
+
                 var bodyData = {
-                    "img" : bannerIMG,
-                    "paragraph" : "Sample Paragraph"
+                    "img" : sprite,
+                    "relations" : getRelations,
+                    "ability" : abilityString,
+                    "paragraph" : getMon
                 };
                 var getNav = req.app.get("getNav");
                 var model = {
                     menuOptions: getNav(),
                     bodyData: bodyData
                 }
-                res.render("landing", model);
+                res.render("pokemon", model);
             }
         }());
     }
@@ -118,9 +127,53 @@ async function hitThatDB(query){
         .then(() => {
             session.close();
             resolve(returnObject);
-        }) 
+        })
     })
 };
 
+async function hitThatDbForRelations(query){
+    const session = driver.session();
+    var returnObject = [];
+    return new Promise(resolve => {
+        session.run(query)
+        .then(result => {
+            for(var i = 0; i < result.records.length; i++){
+                var key = result.records[i]._fields[1].type + "_" +i;
+                    if(result.records[i]._fields[2].properties.name) {
+                        var val = result.records[i]._fields[2].properties.name;
+                    };
+                    if(result.records[i]._fields[2].properties.gen) {
+                        var val = result.records[i]._fields[2].properties.gen;
+                    };
+                    if(result.records[i]._fields[2].properties.legendary){
+                        if(result.records[i]._fields[2].properties.legendary == 0){
+                            var val = "Not Legendary";
+                        }
+                        else {
+                            var val = "Legendary";  
+                        }
+                    };
+                returnObject.push({[key]:val});
+            };
+        })
+        .catch(e => {
+            console.log(e + " WE MESSED UP!!!!")
+        })
+        .then(() => {
+            session.close();
+            console.log(returnObject);
+            resolve(returnObject);
+        })
+    })
+};
+
+
+async function abilityStringMaker(string){
+    var abilityString = string.join("");
+    abilityString = abilityString.slice(2, -2);
+    abilityString = abilityString.split("'");
+    abilityString = abilityString.join(", ");
+    return abilityString;
+};
 
 module.exports = router;
